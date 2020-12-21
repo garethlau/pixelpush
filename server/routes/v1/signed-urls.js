8f8b3174802b58130c4fc2af9becf0ff38cb8dde
Gareth <Gareth.kh.lau@gmail.com>
2020-12-21 15:41:17 -0700

    S3 generate put URL

---
+++ server/routes/v1/signed-urls.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const s3 = require("../../s3");
+const keys = require("../../config/keys");
+
+router.post("/", async (req, res) => {
+  const { key } = req.body;
+  const params = {
+    Key: key,
+    Bucket: keys.S3_BUCKET_NAME,
+  };
+  try {
+    const url = s3.getSignedUrl("putObject", params);
+    return res.status(200).send({ signedUrl: url });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+module.exports = router;

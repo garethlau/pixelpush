e8e11f0e1b2d100c6c2c9cc7b433e9d3b99af0fe
Gareth <Gareth.kh.lau@gmail.com>
2020-12-21 13:28:30 -0700

    Endpoints to create and fetch albums

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/api/health", (_, res) => {
+  return res.status(200).send({ message: "Server is healthy." });
+});
 router.use("/api/v1", require("./v1"));
 
 module.exports = router;
--- server/routes/v1/albums.js
+++ server/routes/v1/albums.js
@@ -1,4 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const Album = mongoose.model("Album");
+
+function generateCode() {
+  let result = "";
+  for (let i = 0; i < 3; i++) {
+    if (i !== 0) result += "-";
+    const buf = crypto.randomBytes(2);
+    const section = buf.toString("hex").toUpperCase();
+    result += section;
+  }
+  return result;
+}
+
+router.post("/", async (req, res) => {
+  const { title, geo } = req.body;
+  try {
+    const code = generateCode();
+    const album = await new Album({ code, title, geo }).save();
+    return res.status(200).send({ album });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+router.get("/:albumCode", async (req, res) => {
+  const { albumCode } = req.params;
+  try {
+    const album = await Album.findOne({ code: albumCode }).exec();
+    if (!album) {
+      return res.status(404).send({ message: "Album not found." });
+    }
+    return res.status(200).send({ album });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
 
 module.exports = router;

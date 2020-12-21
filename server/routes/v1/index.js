874016ef48ff763b6cbe544a3bfa38903e43e2fe
Gareth <Gareth.kh.lau@gmail.com>
2020-12-21 15:41:28 -0700

    Register s3 signed-urls endpoint

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/signed-urls", require("./signed-urls"));
 router.use("/albums", require("./albums"));
 
 module.exports = router;

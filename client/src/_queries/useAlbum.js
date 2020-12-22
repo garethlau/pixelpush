aa86e1ae45c16b9114a6a6161cc8379662603611
Gareth <Gareth.kh.lau@gmail.com>
2020-12-21 18:05:21 -0700

    Custom query to fetch album data

---
+++ client/src/_queries/useAlbum.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+export default function useAlbum(albumCode) {
+  const queryKey = ["album", albumCode];
+  function query() {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`/api/v1/albums/${albumCode}`)
+        .then((response) => {
+          const { album } = response.data;
+          resolve(album);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  const config = {
+    enabled: !!albumCode,
+  };
+
+  return useQuery(queryKey, query, config);
+}

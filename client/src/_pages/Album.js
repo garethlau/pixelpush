b0e0970a38852e2eb347310053660252e5ebf81c
Gareth <Gareth.kh.lau@gmail.com>
2020-12-21 23:41:12 -0700

    Show upload button if user is authenticated

--- client/src/_pages/Album.js
+++ client/src/_pages/Album.js
@@ -21,6 +21,7 @@ import UploadQueue from "../_components/UploadQueue";
 import { UploadQueueContext } from "../_contexts/uploadQueue";
 import { UploadProgressContext } from "../_contexts/uploadProgress";
 import Photo from "../_components/Photo";
+import useUser from "../_queries/useUser";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -54,6 +55,7 @@ export default function Album() {
   const { data: photos, refetch: refetchPhotos } = usePhotos(albumCode);
   const { uploadQueue } = useContext(UploadQueueContext);
   const { progress, setProgress } = useContext(UploadProgressContext);
+  const { data: user } = useUser();
   const queryClient = new QueryClient();
 
   useEffect(() => {
@@ -110,12 +112,14 @@ export default function Album() {
             ))}
           </div>
 
-          <div className={classes.uploadBtn} {...getRootProps()}>
-            <input {...getInputProps()} />
-            <Button variant="contained" color="primary">
-              Upload
-            </Button>
-          </div>
+          {!!user && (
+            <div className={classes.uploadBtn} {...getRootProps()}>
+              <input {...getInputProps()} />
+              <Button variant="contained" color="primary">
+                Upload
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </React.Fragment>
---
+++ client/src/_queries/useUser.js
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+export default function useUser() {
+  const queryKey = "user";
+  function query() {
+    return new Promise((resolve, reject) => {
+      axios
+        .get("/api/v1/me")
+        .then((response) => {
+          const { user } = response.data;
+          resolve(user);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  const config = {};
+
+  return useQuery(queryKey, query, config);
+}

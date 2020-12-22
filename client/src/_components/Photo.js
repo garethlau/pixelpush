284519d2e6a5b89f34c44631efa174e4d841ae78
Gareth <Gareth.kh.lau@gmail.com>
2020-12-22 00:35:14 -0700

    Show actions when hovering over image

--- client/src/_components/Photo.js
+++ client/src/_components/Photo.js
@@ -1,5 +1,12 @@
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import useUser from "../_queries/useUser";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
+import IconButton from "@material-ui/core/IconButton";
+import { useState } from "react";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     position: "relative",
@@ -7,26 +14,50 @@ const useStyles = makeStyles((theme) => ({
   img: {
     width: "100%",
   },
-  dimensions: {
+  actions: {
     position: "absolute",
-    top: 0,
-    left: 0,
-    // backgroundColor: theme.palette.grey[100],
-    backdropFilter: "blur(10px) brightness(0.9)",
-    padding: "5px",
+    right: "10px",
+    top: "50%",
+    backgroundColor: theme.palette.common.white,
+    boxShadow: theme.shadows[10],
+    borderRadius: "5px",
+    transform: "translateY(-50%)",
   },
 }));
 
 export default function Photo({ photo }) {
   const classes = useStyles();
+  const { data: user, isLoading } = useUser();
+  const [showActions, setShowActions] = useState(false);
+
   return (
-    <div className={classes.root}>
+    <div
+      className={classes.root}
+      onMouseEnter={() => setShowActions(true)}
+      onMouseLeave={() => setShowActions(false)}
+    >
       <img className={classes.img} src={photo.url} alt="" />
-      <span className={classes.dimensions}>
-        <Typography variant="caption">
-          {photo.width}x{photo.height}
-        </Typography>
-      </span>
+      {showActions && (
+        <div className={classes.actions}>
+          <div>
+            <IconButton color="primary" size="large">
+              <InfoOutlinedIcon />
+            </IconButton>
+          </div>
+          <div>
+            <IconButton color="primary" size="large">
+              <GetAppIcon />
+            </IconButton>
+          </div>
+          {!isLoading && photo.userId === user?._id && (
+            <div>
+              <IconButton color="primary" size="large">
+                <RemoveCircleOutlineIcon />
+              </IconButton>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }

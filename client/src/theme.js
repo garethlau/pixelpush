be7402379a26319f302552d595fb74510c3ba82d
Gareth <Gareth.kh.lau@gmail.com>
2020-12-21 18:03:56 -0700

    Configure theme

---
+++ client/src/theme.js
@@ -0,0 +1,29 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "DM Sans, sans-serif",
+    fontSize: "16px",
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: "2.125rem",
+    },
+    h3: {
+      fontSize: "1.5rem",
+    },
+    h4: {
+      fontSize: "1.5rem",
+    },
+    h5: {
+      fontSize: "1.5rem",
+    },
+    h6: {
+      fontSize: "1.5rem",
+    },
+  },
+});
+
+export default theme;

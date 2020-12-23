40ce049e756d8c6beaa5d02834c8f0eddd85439e
Gareth <Gareth.kh.lau@gmail.com>
2020-12-23 13:32:49 -0700

    Auto delete Albums after 5 days

--- server/mongo/models/Album.js
+++ server/mongo/models/Album.js
@@ -8,6 +8,7 @@ const AlbumSchema = new Schema({
   photos: Array,
   createdBy: String,
   createdAt: String,
+  _createdAt: { type: Date, expires: "7200m", default: Date.now },
 });
 
 module.exports = mongoose.model("Album", AlbumSchema);

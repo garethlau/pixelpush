a84f4f7425e01109d0c6c46d7a74ec7ed6aa386c
Gareth <Gareth.kh.lau@gmail.com>
2020-12-28 13:42:48 -0700

    Service to subscribe and write to clients

---
+++ server/services/eventSubscription.js
@@ -0,0 +1,62 @@
+let albums = {};
+
+const EventTypes = {
+  IMAGE_LIST_UPDATED: "IMAGE_LIST_UPDATED",
+  ALBUM_DELETED: "ALBUM_DELETED",
+};
+
+function subscribe(req, res) {
+  const { albumCode } = req.params;
+  // Haders and http status to keep connection open
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+  res.writeHead(200, headers);
+
+  const data = `data: ${JSON.stringify({
+    message: "Subscribed to event source",
+  })}\n\n`;
+  res.write(data);
+
+  if (!albums[albumCode]) {
+    albums[albumCode] = [];
+  }
+
+  const clientId = Date.now();
+  const newClient = {
+    id: clientId,
+    res,
+  };
+  albums[albumCode].push(newClient);
+
+  req.on("close", () => {
+    console.log(`${clientId} Connection closed`);
+    albums[albumCode] = albums[albumCode].filter((c) => c.id !== clientId);
+    if (albums[albumCode].length === 0) {
+      let newAlbums = Object.assign({}, albums);
+      delete newAlbums[albumCode];
+      albums = newAlbums;
+    }
+  });
+}
+
+// Wrapper arround sendToClients function for single events without data
+function notifyClients(albumCode, event) {
+  sendToClients(albumCode, { event });
+}
+
+function sendToClients(albumCode, data) {
+  const clients = albums[albumCode];
+  clients.forEach((client) =>
+    client.res.write(`data: ${JSON.stringify(data)}\n\n`)
+  );
+}
+
+module.exports = {
+  sendToClients,
+  notifyClients,
+  subscribe,
+  EventTypes,
+};

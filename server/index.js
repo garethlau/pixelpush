496a413189632179e84eb4f5d2cfcab7f36f6421
Gareth <Gareth.kh.lau@gmail.com>
2020-12-28 13:42:28 -0700

    Set CORS policy and register event subscription route

--- server/index.js
+++ server/index.js
@@ -3,18 +3,33 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
-
+const cors = require("cors");
+const { subscribe } = require("./services/eventSubscription");
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
+
+const whitelist = ["http://localhost:3000", "http://localhost:5000/"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Origin '${origin}' not allowed by cors`));
+    }
+  },
+};
 
 const app = express();
 require("./mongo");
 
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(require("./routes"));
+app.get("/subscribe/:albumCode", subscribe);
 
 app.listen(PORT, () => {
   if (NODE_ENV === "production") {

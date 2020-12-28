658b4e8e7db770230db61bdf376361ecdc901666
Gareth <Gareth.kh.lau@gmail.com>
2020-12-28 14:51:32 -0700

    Disable X-Accel-Buffering

--- server/services/eventSubscription.js
+++ server/services/eventSubscription.js
@@ -12,7 +12,7 @@ function subscribe(req, res) {
     "Content-Type": "text/event-stream",
     Connection: "keep-alive",
     "Cache-Control": "no-cache",
-    "Access-Control-Allow-Origin": "*",
+    "X-Accel-Buffering": "no",
   };
   res.writeHead(200, headers);
 

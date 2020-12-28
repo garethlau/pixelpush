cc38e9d096c448bb0dae93ce8531c7d7ace732b7
Gareth <Gareth.kh.lau@gmail.com>
2020-12-28 15:30:07 -0700

    Add progress handlers for multiple file

--- client/src/_contexts/uploadProgress.js
+++ client/src/_contexts/uploadProgress.js
@@ -4,10 +4,52 @@ export const UploadProgressContext = createContext();
 
 export function UploadProgressProvider({ children }) {
   // Progress is value between 0 and 1
-  const [progress, setProgress] = useState(0);
+  const [uploads, setUploads] = useState({});
+  const [curr, setCurr] = useState(null);
+
+  function startProgress(key) {
+    setUploads((prev) => {
+      prev[key] = 0;
+      return prev;
+    });
+    setCurr(key);
+  }
+
+  function setProgress(key, loaded, total) {
+    setUploads((prev) => ({
+      ...prev,
+      [key]: loaded / total,
+    }));
+  }
+
+  function completeProgress(key) {
+    setUploads((prev) => {
+      let next = Object.assign({}, prev);
+      delete next[key];
+      return next;
+    });
+    setCurr(null);
+  }
+
+  function getProgress(key) {
+    return uploads[key] || 0;
+  }
+
+  function isUploading(key) {
+    return curr === key;
+  }
 
   return (
-    <UploadProgressContext.Provider value={{ progress, setProgress }}>
+    <UploadProgressContext.Provider
+      value={{
+        setProgress,
+        getProgress,
+        completeProgress,
+        startProgress,
+        isUploading,
+        uploads,
+      }}
+    >
       {children}
     </UploadProgressContext.Provider>
   );

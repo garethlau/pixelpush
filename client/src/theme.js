7bab7709a2d788f6f8781bd29d5cdcdbf1875106
Gareth <Gareth.kh.lau@gmail.com>
2021-01-10 01:07:46 -0700

    Override scrollbar styles

--- client/src/theme.js
+++ client/src/theme.js
@@ -24,6 +24,21 @@ const theme = createMuiTheme({
       fontSize: "1.5rem",
     },
   },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "*::-webkit-scrollbar": {
+          width: "0.4em",
+        },
+        "*::-webkit-scrollbar-track": {
+          "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.00)",
+        },
+        "*::-webkit-scrollbar-thumb": {
+          backgroundColor: "rgba(0,0,0,.1)",
+        },
+      },
+    },
+  },
 });
 
 export default theme;

b8d35caa9ce94688429f99d3b0431b1cf3d3d53e
Gareth <Gareth.kh.lau@gmail.com>
2021-01-09 20:41:37 -0700

    Use helper functions to delete photos

---
+++ server/s3/utils.js
@@ -0,0 +1,36 @@
+const keys = require("../config/keys");
+const s3 = require("./index");
+
+async function deletePhoto(key) {
+  const params = {
+    Bucket: keys.S3_BUCKET_NAME,
+    Delete: {
+      Quiet: false,
+      Objects: [{ Key: key }, { Key: key + "-preview" }],
+    },
+  };
+  const result = await s3.deleteObjects(params).promise();
+  return result;
+}
+async function deletePhotos(keys) {
+  let objects = [];
+  keys.forEach((key) => {
+    objects.push({ Key: key });
+    objects.push({ Key: key + "-preview" });
+  });
+  const params = {
+    Bucket: keys.S3_BUCKET_NAME,
+    Delete: {
+      Quiet: false,
+      Objects: [{ Key: key }, { Key: key + "-preview" }],
+    },
+  };
+
+  const result = await s3.deleteObjects(params).promise();
+  return result;
+}
+
+module.exports = {
+  deletePhoto,
+  deletePhotos,
+};
